6131f6e44c9eafff2f349f30e838d7904f3f62b8
JaquelyneMendezHernandez <88173967+JaquelyneMendezHernandez@users.noreply.github.com>
2022-03-29 23:29:14 -0600

    Add files via upload

---
+++ condicionales/condicion/if.js
@@ -0,0 +1,7 @@
+alert("Bienvenido");
+var mensaje ="Te gustan las frutas?";
+var nombreUsuario=prompt(mensaje);
+if (nombreUsuario == "si") 
+alert("Tienes buenos gustos")
+else 
+alert("Derias probarlas")
---
+++ condicionales/condicion/switch_case.js
@@ -0,0 +1,32 @@
+
+var opc=window.prompt("selecciona tu cancion favorita de Gorillaz o selecciona el nombre de tu integrante favorito");
+switch (opc){
+    case('1'):
+    alert('19-2000');
+        break;
+    case('2'):
+    alert('Humility');
+        break;
+    case('3'):
+    alert('Feel Good Inc');
+        break;
+    case('4'):
+    alert('DARE');
+        break;
+    case('2-D'):
+    alert('El vocalista de la banda');
+        break;
+    case('Russel'):
+        alert('El baterista de la banda');
+        break;
+    case('Noodle'):
+    alert('La guitarrista de la banda enviada desde japon');
+        break;
+    case('Murdoc'):
+        alert('El bajista de la banda y lider');
+        break;   
+    default:
+        alert('Error al seleccionar');
+        break;
+}        
+        
